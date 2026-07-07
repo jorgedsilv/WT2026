f1f441c9ea37f95b11890ebff561665fc59c83d8
Jorge Silva <jorgeduartesilva@gmail.com>
2026-07-07 14:54:50 +0100

    Initial Commit

---
+++ WT2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WT2026
+//
+//  Created by Jorge Silva on 07/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WT2026/WT2026App.swift
@@ -0,0 +1,17 @@
+//
+//  WT2026App.swift
+//  WT2026
+//
+//  Created by Jorge Silva on 07/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct WT2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
